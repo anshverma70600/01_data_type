afc1339eaa6de41f2ddf0e62a339f9d88194d593
Ansh Verma <97027597+anshverma70600@users.noreply.github.com>
2024-03-22 07:02:41 +0000

    program

---
+++ 01_input_output/file.cs
@@ -0,0 +1,27 @@
+using System;
+using static System.Console;
+public class data_type{
+    public static void main(){
+        WriteLine("Enter int value : ");
+        int n = Convert.ToInt32(Readline());
+
+        WriteLine("Enter float value : ");
+        float f = Convert.ToSingle(Readline());
+
+        WriteLine("Enter a string : ");
+        string s = Readline();
+
+        WriteLine("Enter bool (true or false) : ");
+        bool b = Convert.ToBoolean(Readline());
+
+        WriteLine("Enter char : ");
+        char c = Convert.ToChar(Readline());
+
+        WriteLine($" {n} is integer !");
+        WriteLine($" {f} is float !");
+        WriteLine($" {s} is string !");
+        WriteLine($" {b} is boolean !");
+        WriteLine($" {c} is char !");
+
+    }
+}
